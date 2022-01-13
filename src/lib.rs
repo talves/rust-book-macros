c6b983af51be9c2fa3dc12092b7a0ffe0459c93f
talves <tony@3alves.com>
2022-01-12 16:46:55 -0800

    init 🔥

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+pub trait HelloMacro {
+    fn hello_macro();
+}
+
+struct Pancakes;
+
+impl HelloMacro for Pancakes {
+    fn hello_macro() {
+        println!("Hello, Macro! My name is Pancakes!");
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!((), crate::Pancakes::hello_macro());
+    }
+}
